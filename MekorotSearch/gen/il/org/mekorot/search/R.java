6b5036f58e99ee83f5dfbcf88bb92306abb9447a
Oren Mishali <oren.mishali@gmail.com>
2013-05-12 09:55:26 +0300

    added buttons to delete the book/path fields.
    These are icon-only buttons (image buttons). we used a delete icon provided by android (accessed via @android/drawable). created borderless button since it looks better. for that had to change the minsdk attribute in the manifest from 8 to 11. btw i not like very much these red icons but will search for better soon. no logic yet for the buttons.

--- MekorotSearch/gen/il/org/mekorot/search/R.java
+++ MekorotSearch/gen/il/org/mekorot/search/R.java
@@ -28,9 +28,11 @@ public final class R {
         public static final int mekorot_logo=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int book=0x7f090001;
-        public static final int path=0x7f090002;
+        public static final int action_settings=0x7f090005;
+        public static final int book=0x7f090002;
+        public static final int book_delete_button=0x7f090001;
+        public static final int path=0x7f090004;
+        public static final int path_delete_button=0x7f090003;
         public static final int webview=0x7f090000;
     }
     public static final class layout {
