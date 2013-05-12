d0c8c9edb81b3789227fcb32e355bf9690822950
Oren Mishali <oren.mishali@gmail.com>
2013-05-12 12:59:12 +0300

    updated the delete icons.
    Some optional icons also added to the stock.

--- MekorotSearch/gen/il/org/mekorot/search/R.java
+++ MekorotSearch/gen/il/org/mekorot/search/R.java
@@ -24,8 +24,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int mekorot_logo=0x7f020001;
+        public static final int delete_icon1=0x7f020000;
+        public static final int delete_icon2=0x7f020001;
+        public static final int delete_icon3=0x7f020002;
+        public static final int delete_icon4=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int mekorot_logo=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f090005;
